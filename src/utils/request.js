fbb03a4e38b4a8b2578a86a5b15d5d58ccf7a2e3
Ilyas Farkhane <125192731+ilyasfarkhane-dev@users.noreply.github.com>
2024-04-15 16:37:45 +0100

    change lien render

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: "https://blogapp-rmn5.onrender.com",
+  baseURL: "https://blogapp-4jvw.onrender.com",
 });
 
 export default request;
